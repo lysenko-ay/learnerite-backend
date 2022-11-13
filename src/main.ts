145385e573a14350b4ff38f2e68c533a9863dee2
AlexTheRegent <alextheregent@gmail.com>
2022-11-13 10:32:31 +0300

    expose app port to config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { mkdirSync } from 'fs';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3030);
+  const configService = app.get<ConfigService>(ConfigService);
+  await app.listen(configService.get('APP_PORT') || 3030);
 }
 mkdirSync('./jobs', { recursive: true });
 bootstrap();
